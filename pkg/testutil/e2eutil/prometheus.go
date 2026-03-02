6cedbc91c9bfa78230d02de4f76602f10a2bb1e1
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2026-03-02 15:59:24 +0100

    update go and deps (#329)
    * update go builder
    
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>
    
    * update bingo tools
    
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>
    
    ---------
    
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- pkg/testutil/e2eutil/prometheus.go
+++ pkg/testutil/e2eutil/prometheus.go
@@ -44,9 +44,9 @@ import (
 )
 
 const (
-	defaultPrometheusVersion   = "v0.37.0"
-	defaultAlertmanagerVersion = "v0.20.0"
-	defaultMinioVersion        = "RELEASE.2022-07-30T05-21-40Z"
+	defaultPrometheusVersion   = "v0.38.0"
+	defaultAlertmanagerVersion = "v0.25.0"
+	defaultMinioVersion        = "RELEASE.2023-01-12T02-06-16Z"
 
 	// Space delimited list of versions.
 	promPathsEnvVar       = "THANOS_TEST_PROMETHEUS_PATHS"
