39f1a9a47e005f5873034b504390fadb454e90ea
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2026-09-18 11:27:56 +0200

    fix: Update Minio image to a mirrored one
    Backport of upstream https://github.com/thanos-io/thanos/pull/9029 (commit f19611d24d36bb82d3134cbf369f3ba3b7bf19a7) to resolve
      broken Docker Hub Minio image pulls in E2E tests.
    
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
    (cherry picked from commit 246402c63bed9205dfa2b4abc717f096da102c4e)

--- test/e2e/query_test.go
+++ test/e2e/query_test.go
@@ -718,7 +718,7 @@ func TestQueryStoreMetrics(t *testing.T) {
 	t.Cleanup(cancel)
 
 	bucket := "store-gw-test"
-	minio := e2edb.NewMinio(e, "thanos-minio", bucket, e2edb.WithMinioTLS())
+	minio := e2edb.NewMinio(e, "thanos-minio", bucket, e2edb.WithMinioTLS(), e2edb.WithImage(e2ethanos.DefaultMinioImage))
 	testutil.Ok(t, e2e.StartAndWaitReady(minio))
 
 	l := log.NewLogfmtLogger(os.Stdout)
@@ -906,7 +906,7 @@ func TestQueryStoreDedup(t *testing.T) {
 	t.Cleanup(cancel)
 
 	bucket := "store-gw-dedup-test"
-	minio := e2edb.NewMinio(e, "thanos-minio", bucket, e2edb.WithMinioTLS())
+	minio := e2edb.NewMinio(e, "thanos-minio", bucket, e2edb.WithMinioTLS(), e2edb.WithImage(e2ethanos.DefaultMinioImage))
 	testutil.Ok(t, e2e.StartAndWaitReady(minio))
 
 	l := log.NewLogfmtLogger(os.Stdout)
@@ -2139,7 +2139,7 @@ func TestQueryTenancyEnforcement(t *testing.T) {
 	t.Cleanup(cancel)
 
 	bucket := "store-gw-test"
-	minio := e2edb.NewMinio(e, "thanos-minio", bucket, e2edb.WithMinioTLS())
+	minio := e2edb.NewMinio(e, "thanos-minio", bucket, e2edb.WithMinioTLS(), e2edb.WithImage(e2ethanos.DefaultMinioImage))
 	testutil.Ok(t, e2e.StartAndWaitReady(minio))
 
 	l := log.NewLogfmtLogger(os.Stdout)
